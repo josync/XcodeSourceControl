d2a638e0255b993a538979d86999bd53013f0bd6
Hyunsung Jo <josync@naver.com>
2025-06-19 15:12:46 +0900

    Initial Commit

---
+++ XcodeSourceControl/XcodeSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeSourceControlApp.swift
+//  XcodeSourceControl
+//
+//  Created by HyunsungX on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct XcodeSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
