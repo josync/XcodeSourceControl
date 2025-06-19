ca3eae81633e1a341c9511b99175ee801bc3cd98
Hyunsung Jo <josync@naver.com>
2025-06-19 18:45:36 +0900

    블로그 포스팅을 위한 신규 파일 SyncView 생성

---
+++ XcodeSourceControl/SyncView.swift
@@ -0,0 +1,18 @@
+//
+//  SyncView.swift
+//  XcodeSourceControl
+//
+//  Created by HyunsungX on 6/19/25.
+//
+
+import SwiftUI
+
+struct SyncView: View {
+    var body: some View {
+        Text("Hello, Josync!")
+    }
+}
+
+#Preview {
+    SyncView()
+}
