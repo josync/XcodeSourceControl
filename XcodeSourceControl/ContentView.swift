239fa619b56b360c69987f3a1ed8eb3b78abc02c
Hyunsung Jo <josync@naver.com>
2025-06-21 12:54:03 +0900

    [기능] 로그아웃 완료

--- XcodeSourceControl/ContentView.swift
+++ XcodeSourceControl/ContentView.swift
@@ -10,8 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            // auth : 로그인 기능 완료
-            // auth : 로그인 완료 후 콘텐츠뷰로 자동 전환
+            // auth(커밋✅) : 로그인 기능 완료
+            // auth(커밋✅) : 로그인 완료 후 콘텐츠뷰로 자동 전환
+            // auth(커밋✅) : 로그아웃 기능 완료
         }
         .padding()
     }
