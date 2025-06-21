c79de36b9900dd092f8f19b23c822f73a02cf032
Hyunsung Jo <josync@naver.com>
2025-06-21 10:08:52 +0900

    신규 JoView.swift 파일 생성

---
+++ XcodeSourceControl/JoView.swift
@@ -0,0 +1,18 @@
+//
+//  JoView.swift
+//  XcodeSourceControl
+//
+//  Created by HyunsungX on 6/21/25.
+//
+
+import SwiftUI
+
+struct JoView: View {
+    var body: some View {
+        Text("Jo, World!")
+    }
+}
+
+#Preview {
+    JoView()
+}
